a217d8e4bafef95dea2904a3e4ec85b8d0e3da8b
MudasirQBhat <mudasirmailg@gmail.com>
2024-11-17 15:28:57 +0530

    Initial commit

---
+++ data/todos.ts
@@ -0,0 +1,53 @@
+export const data = [
+    {
+      "id": 1,
+      "title": "Buy groceries",
+      "completed": false
+    },
+    {
+      "id": 2,
+      "title": "Clean the house",
+      "completed": true
+    },
+    {
+      "id": 3,
+      "title": "Prepare presentation",
+      "completed": false
+    },
+    {
+      "id": 4,
+      "title": "Call the plumber",
+      "completed": true
+    },
+    {
+      "id": 5,
+      "title": "Finish coding project",
+      "completed": false
+    },
+    {
+      "id": 6,
+      "title": "Go for a run",
+      "completed": true
+    },
+    {
+      "id": 7,
+      "title": "Read a book",
+      "completed": false
+    },
+    {
+      "id": 8,
+      "title": "Plan weekend trip",
+      "completed": true
+    },
+    {
+      "id": 9,
+      "title": "Fix the bike",
+      "completed": false
+    },
+    {
+      "id": 10,
+      "title": "Respond to emails",
+      "completed": true
+    }
+  ]
+  
